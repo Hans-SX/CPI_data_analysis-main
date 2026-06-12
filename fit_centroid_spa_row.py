359e344c93c9093f5bbfb8ca14a2fb9cbd58e24a
unknown <you@example.com>
2026-06-12 13:26:41 +0200

    sampling points from weighted average of angular for fitting.

---
+++ fit_centroid_spa_row.py
@@ -0,0 +1,82 @@
+import argparse
+import os
+from os.path import join
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+from utils import Calculating_G2, line, column_centroids
+from CPI import readConfig, setDirectories_twocams
+from temp import BigStepForward_SmallStepBack, mean_positions_per_second
+from config import shift, binA, binB, z_of_slope
+
+"""
+It did not worked as expected.
+"""
+
+exec(readConfig())
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--DataSet', type=str)
+parser.add_argument('--refName', nargs='?', default='refocused', type=str)
+args = parser.parse_args()
+
+datapath = join(os.getcwd(), os.pardir, args.DataSet, 'data')
+outpath = join(os.getcwd(), os.pardir, args.DataSet, args.refName)
+outDir, armAfiles, armBfiles = setDirectories_twocams(stdData=STD_PATH, stdOut=STD_PATH, timeTag=TT_BOOL, dataPath=datapath, outPath=outpath, armA=armA_PATH, armB=armB_PATH)
+
+pattern = mean_positions_per_second(shift, BigStepForward_SmallStepBack(6.8, 12.8, pattern=np.array((6, -3))).pos_frames(interval=0.1, time_interval=50)['pos'], speed=0.1)
+try_ref_to = pattern[0]
+expect_ref = pattern[1]
+
+avg_z = []
+cyc = 0
+for Afile, Bfile, exp in zip(armAfiles, armBfiles, expect_ref):
+# for Afile, Bfile in zip(armAfiles, armBfiles):
+    arr = Calculating_G2(Afile, Bfile, frames=50)
+    G2 = arr.correlation(binA, binB)
+
+    xaxb = np.sum(G2, axis=(1,3))
+    yayb = np.sum(G2, axis=(0,2))
+    xaxb[np.where(xaxb < 0)] = 0
+    yayb[np.where(yayb < 0)] = 0
+
+    centroids_x = column_centroids(xaxb)
+    centroids_y = column_centroids(yayb)
+
+    range_x = np.where(np.sum(xaxb, axis=0) > 0.6 * np.max(np.sum(xaxb, axis=0)))[0]
+    range_y = np.where(np.sum(yayb, axis=0) > 0.6 * np.max(np.sum(yayb, axis=0)))[0]
+    # range_x = np.arange(30, 70)
+    # range_y = np.arange(30, 70)
+    x_popt, x_pcov = curve_fit(line, range_x, centroids_x[range_x])
+    y_popt, y_pcov = curve_fit(line, range_y, centroids_y[range_y])
+
+    x_line = line(range(xaxb.shape[1]), *x_popt)
+    y_line = line(range(yayb.shape[1]), *y_popt)
+
+    z_from_x = z_of_slope(x_popt[0])
+    z_from_y = z_of_slope(y_popt[0])
+    avg_z.append((z_from_x + z_from_y) / 2)
+
+    fig, (ax1,ax2) = plt.subplots(2,figsize=(6,10))
+    im1 = ax1.imshow(xaxb, cmap="gray")
+    ax1.plot(x_line, color="red")
+    ax1.scatter(range(centroids_x.shape[0]), centroids_x, color="blue")
+    im2 = ax2.imshow(yayb, cmap="gray")
+    ax2.plot(y_line, color="red")
+    ax2.scatter(range(centroids_y.shape[0]), centroids_y, color="blue")
+    ax1.set_title(f"xA-xB Corr. Func., z from xaxb: {round(z_from_x, 3)} mm")
+    ax2.set_title(f"yA-yB Corr. Func., z from yayb: {round(z_from_y, 3)} mm")
+    fig.savefig(join(outDir, str(cyc + 1) + f"_expected_z_{exp:03f}_mm.png"), dpi='figure',
+    transparent=False)
+    plt.close("all")
+
+    cyc += 1
+
+fig = plt.figure()
+plt.plot(range(1, 1+len(expect_ref)), expect_ref, color='black', label='Expected Z')
+plt.scatter(range(1, 1+len(expect_ref)), avg_z, label='Average Z from Slope')
+plt.title('Axial Position Analysis')
+plt.legend()
+fig.savefig(join(outDir, "z_from_slope.png"), dpi='figure', transparent=False)
+plt.close("all")
